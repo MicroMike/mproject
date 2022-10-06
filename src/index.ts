176811aee74fac9201229f2252377e63026874fa
Mike <assoune.mike@gmail.com>
2022-10-06 15:57:03 +0200

    c

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@
 import { wait } from './helpers/helpers'
 import shell from 'shelljs'
 
-const [a, b, arg, max, checkAccount] = process.argv
-const promises: Promise<unknown>[] = []
+const props = process.argv
+const arg = props[2]
+const max = props[3] || 1
+const checkAccount = props[4] || ''
 
 shell.exec('rm -rf /root/puppet/puppet/')
 
 const infiniteLoop = async () => {
 	// await go(process.argv)
-	await shell.exec(`node build/go.js ${arg} ${max} ${checkAccount || ''}`)
+	await shell.exec(`node build/go.js ${arg} ${max} ${checkAccount}`)
 	await wait(5000)
 	await infiniteLoop()
 }
