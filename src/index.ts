bfb3c380154d01ee87d0de3471631a83c96ed88b
Mike <assoune.mike@gmail.com>
2022-10-23 15:37:20 +0200

    c

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ shell.exec('rm -rf /root/puppet/puppet/')
 shell.exec('killall chrome')
 
 const status = Array(max).fill(false)
-const pids = Array(max).fill([0])
+const pids = [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0]]
 
 // @ts-ignore
 process.env.pids = pids
