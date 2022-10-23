d41788aa345d369cda607e0a711569713e86ba96
Mike <assoune.mike@gmail.com>
2022-10-23 16:56:43 +0200

    c

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,6 @@ const arg = props[2]
 const max = Number(props[3] || 1)
 const checkAccount = props[4] || 'none'
 
-shell.exec('rm -rf /root/puppet/puppet/')
-shell.exec('killall chrome')
-
 const status = Array(max).fill(false)
 
 const infiniteLoop = async (i: number) => {
@@ -23,18 +20,22 @@ const infiniteLoop = async (i: number) => {
 	// })
 }
 
-Array(max).fill(0).forEach((a, i) => {
-	process.env[`pid${i}`] = ''
+shell.exec('rm -rf /root/puppet/puppet/', () => {
+	shell.exec('killall chrome', () => {
+		Array(max).fill(0).forEach((a, i) => {
+			process.env[`pid${i}`] = ''
 
-	if (!status[i]) {
-		status[i] = true
-		infiniteLoop(i)
-	}
+			if (!status[i]) {
+				status[i] = true
+				infiniteLoop(i)
+			}
 
-	setInterval(() => {
-		if (!status[i]) {
-			status[i] = true
-			infiniteLoop(i)
-		}
-	}, 1000 * 60 * 5)
+			setInterval(() => {
+				if (!status[i]) {
+					status[i] = true
+					infiniteLoop(i)
+				}
+			}, 1000 * 60 * 5)
+		})
+	})
 })
