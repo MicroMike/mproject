aae2665d15b08a298172e48754e2bd86a9c32e3e
Mike <assoune.mike@gmail.com>
2022-10-23 16:48:51 +0200

    c

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ Array(max).fill(0).forEach((a, i) => {
 })
 
 const infiniteLoop = async (i: number) => {
+	console.log('go', String(i))
 	await go(process.argv, String(i))
 	// shell.exec(`node build/go.js ${arg} ${max} ${checkAccount} ${i}`, async () => {
 	// shell.exec('git pull')
