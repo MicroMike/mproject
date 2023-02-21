96434929929afd0f63c413f0be5030e17e9be727
Mike <assoune.mike@gmail.com>
2023-02-22 00:52:22 +0100

    c

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ const infiniteLoop = async (i: number) => {
 	// await go(process.argv, String(i))
 	// process.env[`pid${i}`] = ''
 	shell.exec(`tsc && node build/go.js ${arg} ${max} ${checkAccount} ${i}`, async () => {
-		shell.exec('git reset --hard')
+		// shell.exec('git reset --hard')
 		shell.exec('git pull')
 		status[i] = false
 	})
