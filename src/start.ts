1da99a6990839153a2e06dfee9904cbecf56cc70
Mike <assoune.mike@gmail.com>
2023-03-17 11:22:41 +0100

    c

--- src/start.ts
+++ src/start.ts
@@ -11,7 +11,7 @@ export const start = (props: any, chrome: any, protocol: any) => new Promise(asy
 	let countPlays = 0
 	let pauseCount = 0
 	let out: any = false
-	let playByLoop = 2
+	let playByLoop = rand(7, 2)
 	let playLoop = 0
 	let timeout: any
 
