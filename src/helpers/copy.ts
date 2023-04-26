3c726820c00a36cda7fd43866c81e599c4ef10a4
Mike <assoune.mike@gmail.com>
2023-04-26 14:16:21 +0200

    c

--- src/helpers/copy.ts
+++ src/helpers/copy.ts
@@ -19,6 +19,8 @@ export const getSession = (player: string, login: string) => new Promise((res, r
 	if (isYoutube) {
 		// shell.exec(`mkdir -p /root/puppet/puppet/${folder}`, { silent: true })
 		shell.exec(`scp -r root@${serverIp}:"/root/puppet/${folder}" /root/puppet/puppet/${folder}`, { silent: true }, () => {
+			// @ts-ignore
+			console.log('END getSession'.green, player, login)
 			res(true)
 		})
 	} else {
@@ -28,12 +30,15 @@ export const getSession = (player: string, login: string) => new Promise((res, r
 		shell.exec(`scp -r root@${serverIp}:"/root/puppet/${folder}/Default/Login\\ Data\\ For\\ Account" /root/puppet/puppet/${folder}/Default/`, { silent: true })
 		shell.exec(`scp -r root@${serverIp}:"/root/puppet/${folder}/Default/Login\\ Data" /root/puppet/puppet/${folder}/Default/`, { silent: true })
 		shell.exec(`scp -r root@${serverIp}:"/root/puppet/${folder}/Default/Cookies" /root/puppet/puppet/${folder}/Default/`, { silent: true })
+
+		// @ts-ignore
+		console.log('END getSession'.green, player, login)
+
 		res(true)
 	}
 
 	// @ts-ignore
 	console.log('END getSession'.green, player, login)
-
 })
 
 export const copyBack = (player: string, login: string) => new Promise((res, rej) => {
@@ -52,6 +57,8 @@ export const copyBack = (player: string, login: string) => new Promise((res, rej
 	if (isYoutube) {
 		// shell.exec(`ssh root@${serverIp} mkdir -p /root/puppet/${folder}`, { silent: true })
 		shell.exec(`scp -r /root/puppet/puppet/${folder} root@${serverIp}:"/root/puppet"`, { silent: true }, () => {
+			// @ts-ignore
+			console.log('END copyBack'.green, player, login)
 			res(true)
 		})
 	} else {
@@ -61,10 +68,12 @@ export const copyBack = (player: string, login: string) => new Promise((res, rej
 		shell.exec(`scp -r /root/puppet/puppet/${folder}/Default/Login\\ Data\\ For\\ Account root@${serverIp}:"/root/puppet/${folder}/Default/"`, { silent: true })
 		shell.exec(`scp -r /root/puppet/puppet/${folder}/Default/Login\\ Data root@${serverIp}:"/root/puppet/${folder}/Default/"`, { silent: true })
 		shell.exec(`scp -r /root/puppet/puppet/${folder}/Default/Cookies root@${serverIp}:"/root/puppet/${folder}/Default/"`, { silent: true })
+
+		// @ts-ignore
+		console.log('END copyBack'.green, player, login)
+
 		res(true)
 	}
 
-	// @ts-ignore
-	console.log('END copyBack'.green, player, login)
 
 })
