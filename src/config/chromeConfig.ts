371b5c25abbc58b26f3dfd995a6e919d1cc1986d
Mike <assoune.mike@gmail.com>
2023-04-27 23:02:20 +0200

    c

--- src/config/chromeConfig.ts
+++ src/config/chromeConfig.ts
@@ -3,9 +3,8 @@ import { rand } from "../helpers/helpers";
 const chromeLauncher = require('chrome-launcher');
 
 const uAgents = [
-	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
-	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
-	'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+	'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
 ]
 
 export const chromeConfig = (player?: string, login?: string) => {
