53dc47ec32fc4699ff53a9059c847c61601dcef9
Mike <assoune.mike@gmail.com>
2023-04-30 10:33:25 +0200

    c

--- src/config/chromeConfig.ts
+++ src/config/chromeConfig.ts
@@ -19,6 +19,7 @@ export const chromeConfig = (player?: string, login?: string) => {
 				'--disable-prompt-on-repost',
 				'--deny-permission-prompts',
 				'--noerrdialogs',
+				'--restore-last-session',
 				// `--user-agent=${uAgents[rand(uAgents.length - 1)]}`,
 				player && login && '--user-data-dir=/root/puppet/puppet/' + player + login,
 				// '--disable-setuid-sandbox',
