b547a44b1479713df5a181d6d5497e9f0b989f1d
Mike <assoune.mike@gmail.com>
2023-05-12 01:00:28 +0200

    c

--- src/userConnect.ts
+++ src/userConnect.ts
@@ -155,8 +155,8 @@ export const userConnect = async ({ P, R, I, S, account, check, socketEmit, coun
 						await press(I, 'Tab')
 						await press(I, 'Tab')
 						await press(I, 'Tab')
-						await press(I, 'Tab')
-						await press(I, 'Tab')
+						!isSpotify && await press(I, 'Tab')
+						!isSpotify && await press(I, 'Tab')
 
 
 						await wait(rand(5, 3) * 1000)
